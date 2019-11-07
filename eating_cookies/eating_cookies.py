95d12fc5963bc243a877a6a98e65198caf4b69dc
Zolton <gmzesq@gmail.com>
2019-11-07 17:07:42 -0500

    Added fibonacci formula; tribonacci WIP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -30,6 +30,45 @@ def eating_cookies(N):
   return count[N]
 print(eating_cookies(10))
 
+#!/usr/bin/python
+
+import sys
+import math
+
+#Fibonacci sequence in O(1) runtime:
+def fibonacci(n):
+  gamma = math.sqrt(5)
+  alpha = ((1+gamma)/2)**n
+  beta = ((1-gamma)/2)**n
+  answer = (alpha - beta)/gamma
+  print(round(answer))
+print(fibonacci(500))
+
+#x** (1. / 3)
+def tribonacci(n):
+  test1 = (3*math.sqrt(33))
+  gamma1 = (19-test1)**(1./3.)
+  gamma2 = (19+test1)**(1./3.)
+  test5 = 1+gamma1+gamma2
+  theta = test5/3
+  logTheta = math.log(theta)
+  print("theta", theta)
+  test9 = (theta**n)/44
+  print ("test9", test9)
+  logTen = math.log(10000)
+  print("theta log", logTheta)
+  answer = (n*logTheta - logTen)
+
+print(tribonacci(7))
+
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_cookies = int(sys.argv[1])
+    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+  else:
+    print('Usage: eating_cookies.py [num_cookies]')
+
 if __name__ == "__main__":
   if len(sys.argv) > 1:
     num_cookies = int(sys.argv[1])
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,19 @@
 import sys
 
 def rock_paper_scissors(n):
-  possible = ["rock", "paper", "scissors"]
+  # possible = ["rock", "paper", "scissors"]
+  # count = 0
+  # solution = []
+  # if count >= n:
+  #   rock_paper_scissors
+  # else:
+  #   count = count + 1
+  #   for 
+  # for i in range(len(possible)):
+  #   for j in possible:
+  #     print([possible[j]])
+      #print(i)
+      #print(j)
 
 
 print(rock_paper_scissors(2))
