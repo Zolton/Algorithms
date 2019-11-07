50ac8670f527063c4db949411e7da881e458e9fa
Zolton <gmzesq@gmail.com>
2019-11-06 20:23:56 -0500

    3rd algorithm done, MVP hit

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,48 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  # x = recipe.values()
-  # print("Recipes: ", recipe.values()) 
-  # print("Ingredients: ", ingredients) 
-  #my_dict ={"java":100, "python":112, "c":11} 
-  
-# list out keys and values separately 
   recipeKeys = list(recipe.keys()) 
   recipeValues = list(recipe.values()) 
   ingredientsKeys = list(ingredients.keys()) 
   ingredientsValues = list(ingredients.values()) 
-  print(recipeKeys)
-  print(recipeValues)
-  print(ingredientsKeys)
-  print(ingredientsValues)
-  count = 0
+  count = []
   if recipeKeys != ingredientsKeys:
     return 0
   else:
-    for i in recipeValues:
-      for j in ingredientsValues:
-        print("thi sis i: ", i)
-        print("thi sis j: ", j)
-        # if ingredientsValues[i] / recipeValues[i] <= 0:
-        #   return count
-        if j/i > 0:
-          count = count + 1
-          print("test", count)
-  return count
-  
-    # for x in 
-    #   print("There's enough")
-  # for x in recipe:
-  #   # if recipe.values() >= ingredients.values():
-  #   print("Name is ", x.values())
-  #   print("value is ", x.keys())
-  #   for i in ingredients:
-  #     print(i)
-  #     if x > i:
-  #       print(str(x) + "is greater than " + str(i) + " ok")
-  #     if x < i:
-  #       print(str(x) + "is not greater than " + str(i) + "not ok")
-  
+    for i in range(0, len(recipeValues)):
+      test = ingredientsValues[i] / recipeValues[i]
+      print("test", test)
+      if test > 1:
+        count.append(round(test))
+  return min(count)
 
 
 if __name__ == '__main__':
